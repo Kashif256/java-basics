dda893f0a6c5fbf5ba23c645d40e65e50a15bec5
Kashif Ulhaq K <kashifulhaq751@gmail.com>
2026-07-23 10:48:40 +0530

    Datatypes

---
+++ DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // int
+        int a = 10;
+        int b = 20;
+
+        System.out.println("The value of a is: " + a);
+        System.out.println("The value of b is: " + b);
+
+        // float
+        float c = 10.5f;
+        float d = 20.5f;
+
+        System.out.println("The value of c + d is: " + (c + d));
+
+        // char
+        char grade = 'A';
+        char initial = 'K';
+
+        System.out.println("The grade is: " + grade);
+        System.out.println("My initial is: " + initial);
+    }
+}
