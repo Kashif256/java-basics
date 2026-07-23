6b8012980281ea7a0aeb50ca1d1ad5f5ceeaafb3
Kashif Ulhaq K <kashifulhaq751@gmail.com>
2026-07-23 13:04:12 +0530

    if-else

---
+++ ifelse.java
@@ -0,0 +1,33 @@
+public class ifelse {
+    public static void main(String[] args){
+
+        int k = 20;
+        int s = 30;
+        /*if(k<s){
+            System.out.println("The value of k is " + k + " and the value of s is " + s + " so the value of k is less than s"); // The value of k is20 and the value of s is 30 so the value of k is less than s
+        }*/
+      /*if( k == s){
+            System.out.println("The value is equal");
+        }
+        else{
+            System.out.println("The value is not equal");
+        } // The value is not equal  */
+
+        if(k>s){
+            System.out.println("The value k is greater than s"); // The value of k is greater than s
+        }
+        else{
+            System.out.println("The value of s is greater than k");
+        }
+
+
+        int s1= 50;
+        int s2= 60;
+        if(s1 == s2){
+               System.out.println("The value of s1 is equal to s2");
+        }
+        else if ( s1 != s2){
+            System.out.println("The value of s1 is not equal to s2");
+        }    
+    }
+}
