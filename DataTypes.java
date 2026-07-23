b2f898648a559c176b6682c7ef2061333266d764
Kashif Ulhaq K <kashifulhaq751@gmail.com>
2026-07-23 11:07:03 +0530

    Few new updation of  Datatypes

--- DataTypes.java
+++ DataTypes.java
@@ -16,10 +16,38 @@ public class DataTypes {
         System.out.println("The value of c + d is: " + (c + d));
 
         // char
-        char grade = 'A';
+        char grade = 'A'; //All the characters are enclosed in single quotes
         char initial = 'K';
 
         System.out.println("The grade is: " + grade);
         System.out.println("My initial is: " + initial);
+
+        // double
+        double e = 40.40 ; 
+        System.out.println("The value of e is: " + e);
+
+        // boolean
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println("The value of b1 is: " + b1);
+        System.out.println("The value of b2 is: " + b2);
+
+        // String
+        // it is a non-primitive data type
+        //A String is actually an object created from the String class.
+        String k = "Kash"; // String should be written in double quotes
+        System.out.println("My name is: " + k);
+
+
+        /*An example for learning why i cant use char for strings 
+        As because in char we can store only one char but in string we can store multiple characters */
+        // example program
+        
+        char firstLetter = 'K';
+        String fullName = "Kashif";
+
+        System.out.println(firstLetter); // K
+        System.out.println(fullName);    // Kashif
+
     }
 }
