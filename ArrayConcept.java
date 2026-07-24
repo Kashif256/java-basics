a1dd4af136c0c052413b1f54a1bc2972cebb2579
Kashif Ulhaq K <kashifulhaq751@gmail.com>
2026-07-24 21:15:41 +0530

    ArraysConcept with object class

--- ArrayConcept.java
+++ ArrayConcept.java
@@ -1,5 +1,13 @@
 public class ArrayConcept {
     // An array in Java is a data structure that stores multiple values of the same data type in a single variable. Each value is stored at a specific position called an index, starting from 0.
+    
+    // Objet array also in this file
+
+    // disadvantage
+    /*
+    1.In array the size is fixed --> Static array-- To overcome this problem we use COLLECTION--ARRAY LIST, HASHTABLE --> Dynamic array
+    2. Stores only similar value (char,string, int, double float) can be in an single array --- To overcome this we use OBJECT ARRAY
+     */
     public static void main(String[] args){
         int i[] = new int[4]; // index value is 4 and starts from 0,1,2,and 3 because n-1
         i[0]=10;
@@ -20,7 +28,6 @@ public class ArrayConcept {
             System.out.println("The value of i is: " + i[j]);
         }
 
-
         // this is an method or we can have an another method also for performing the array operation
 
         int[] num ={10,20,30,40};
@@ -30,5 +37,36 @@ public class ArrayConcept {
             System.out.println("The value of num is:" + num[k]);
         }
 
+        //Double array
+
+        double d[] = new double [3];
+        d[0] = 10.02;
+        d[1] = 20.23;
+        d[2] = 30.87;
+
+        System.out.println("The value of index 0 is: " + (d[0] + d[1])); // array inside concatenation
+
+        System.out.println("The value of index 1 is: " + d[1]);
+        System.out.println("The value of index 2 is: " + d[2]);
+
+        // Object
+        // object is a super class of all the class
+        // In object we can store multiple values
+
+        Object ob[] = new Object[6];
+
+        ob[0] = "Kashif";
+        ob[1] = "21";
+        ob[2] = "CSE";
+        ob[3] = "Final Year";
+        ob[5] = "Kash" ;
+        ob[4] = "100004700.047";
+        
+        System.out.println(ob[0]);
+        System.out.println(ob[1]);
+        System.out.println(ob[2]);
+        System.out.println(ob[3]);
+        System.out.println(ob[5]);
+        System.out.println(ob[4]);
     }
 }
