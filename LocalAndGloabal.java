10b61bc57617966bd3697ae35e2e6de6c6b7a9f3
Kashif Ulhaq K <kashifulhaq751@gmail.com>
2026-07-27 21:07:50 +0530

    LocalAndGlobal

---
+++ LocalAndGloabal.java
@@ -0,0 +1,23 @@
+public class LocalAndGloabal {
+    // Global variable - class variable it is palced immediately after the class
+    String name = "Kashif";
+    int age = 17;
+    public static void main (String[] args) {
+        int i = 10;  // Local variable
+        System.out.println(i);
+
+        LocalAndGloabal obj = new LocalAndGloabal(); // if we want to use the gloabal variable inside the local variable then we have to create a object
+        System.out.println(obj.name);
+        System.out.println(obj.age);
+        obj.sum(); // here is the calling for sum 
+        
+    }
+
+    public void sum(){
+        int i =1000;
+        int k = 2000;
+        System.out.println(i); // sum() is a non-static method, you must call it using an object.
+        System.out.println(k); 
+    }
+    
+   }
